02048d1f28bbfa40caa3aaf4b4d4e1cf050819b8
chanhalzkie <45038597+chanhalzkie@users.noreply.github.com>
2022-04-04 05:07:22 +0800

    Add files via upload

---
+++ userscript.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         JnP Logo template
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  JnP on r/place
+// @author       ChanhaLzkie
+// ==/UserScript==
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            const i = document.createElement("img");
+            i.src = "https://raw.githubusercontent.com/chanhalzkie/jnp/main/jnp_template.png";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
+            console.log(i);
+            return i;
+        })())
+
+    }, false);
+
+}
